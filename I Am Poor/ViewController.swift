f8df68f49e1d6df318dc52c17102ca61a65ad2b2
batmandaksh <batmandaksh@gmail.com>
2025-03-21 20:42:31 +0530

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Daksh Prashar on 2025-03-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
